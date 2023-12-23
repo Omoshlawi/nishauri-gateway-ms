185ab1678cff97eee503d02341c5e1530a4b2ccf
Omoshlawi <lawiomosh3@gmail.com>
2023-12-23 12:38:37 +0300

    Added folder structure template, also made connection to db and added cores, json, static and morgan middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,40 @@
 // src/index.ts
-import express from "express";
 import "dotenv/config";
-const app = express();
 import config from "config";
+import express from "express";
+import proxy from "express-http-proxy";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+import { MEDIA_ROOT } from "./utils";
+
+const app = express();
+
+// connect to database
+mongoose
+  .connect(config.get("db"))
+  .then((result) => {
+    console.log(`[+]${config.get("name")} Connected to database Successfully`);
+  })
+  .catch((err) => {
+    console.log("[x]Could not connect to database" + err);
+    process.exit(1); // Exit the application on database connection error
+  });
+
+// middlewares
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log(`[+]Morgan logger Enabled for ${config.get("name")}`);
+}
+app.use(cors());
+app.use(express.json());
+app.use(express.static(MEDIA_ROOT));
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send({ data: "Hello, world!" });
 });
 
 const port = config.get("port");
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
+  console.log(`[+]${config.get("name")} listening on port ${port}...`);
 });
