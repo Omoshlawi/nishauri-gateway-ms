972fdd791ddb1ac4d1a9738ce2023d130e5ddec9
Omoshlawi <lawiomosh3@gmail.com>
2023-12-30 16:41:54 +0300

    Added patients feature with correct folder and file sructure, also done with programs repo for crude

---
+++ src/features/patients/repositories/programs.ts
@@ -0,0 +1,38 @@
+import ServiceClient from "../../../shared/ServiceClient";
+
+const getPrograms = async () => {
+  return await ServiceClient.callService("nishauri-patients-ms", {
+    method: "GET",
+    url: "programs",
+  });
+};
+
+const addProgram = async (data: any, token: string) => {
+  return await ServiceClient.callService("nishauri-patients-ms", {
+    method: "POST",
+    url: "programs",
+    data,
+  });
+};
+
+const updateProgram = async (programId: string, data: any, token: string) => {
+  return await ServiceClient.callService("nishauri-patients-ms", {
+    method: "PUT",
+    url: `programs/${programId}`,
+    data,
+  });
+};
+
+const deleteProgram = async (programId: string, data: any, token: string) => {
+  return await ServiceClient.callService("nishauri-patients-ms", {
+    method: "DELETE",
+    url: `programs/${programId}`,
+  });
+};
+
+export default {
+  getPrograms,
+  addProgram,
+  updateProgram,
+  deleteProgram,
+};
